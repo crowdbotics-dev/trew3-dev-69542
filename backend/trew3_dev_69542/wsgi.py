eb70655e99513244e13d516f69301380a9e8cd42
Crowdbotics <engineering@crowdbotics.com>
2023-03-15 06:21:13 +0000

    Initial commit.

---
+++ backend/trew3_dev_69542/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for trew3_dev_69542 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trew3_dev_69542.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "trew3-dev-69542",
+  url: "https://trew3-dev-69542.botics.co",
+  api: "https://trew3-dev-69542.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
